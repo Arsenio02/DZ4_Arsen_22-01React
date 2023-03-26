876aff2123b4a6ea3525660c6fe472a3ce10f853
Arsen0203 <matosmon0203@gmail.com>
2023-03-27 00:37:31 +0600

    react honework4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const RegistrationForm = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        phone: '',
+        yearOfBirth: ''
+    });
 
-export default App;
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(`Name: ${formData.name}\nPhone: ${formData.phone}\nYear of birth: ${formData.yearOfBirth}`);
+        console.log(formData);
+    };
+
+    return (
+        <form className="registration-form" onSubmit={handleSubmit}>
+            <h2>Registration Form</h2>
+            <div className="form-group">
+                <label htmlFor="name">Name:</label>
+                <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
+            </div>
+            <div className="form-group">
+                <label htmlFor="phone">Phone:</label>
+                <input type="tel" id="phone" name="phone" value={formData.phone} onChange={handleChange} required />
+            </div>
+            <div className="form-group">
+                <label htmlFor="year-of-birth">Year of Birth:</label>
+                <input type="number" id="year-of-birth" name="yearOfBirth" value={formData.yearOfBirth} onChange={handleChange} required />
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    );
+};
+
+export default RegistrationForm;
